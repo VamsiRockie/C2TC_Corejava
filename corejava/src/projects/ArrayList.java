2b30366ac20482d69f4158384ec6789943b24f26
vamsi <vamsi@DESKTOP-9RC713Q>
2022-06-14 18:16:02 +0530

    commit

---
+++ corejava/src/projects/ArrayList.java
@@ -0,0 +1,38 @@
+package projects;
+
+	import java.util.ArrayList; import java.util.List;
+
+	public class ArrayList
+	{
+	public static void main(String[] args)
+	{
+	// Creates an ArrayList object with an initial capacity of 10. List<String> list = new ArrayList<String>();
+
+	// Call add() method to add elements at the end of the list using the reference
+
+	variable list.
+
+	System.out.println("Adding elements to end of list");
+
+	list.add("A"); // Adding elemen at index 0. list.add("B"); // Adding element at index 1.
+
+	list.add("D"); // Adding element at index 2.
+
+	list.add("E"); // Adding element at index 3. list.add("G"); // Adding element at index 4.
+
+	System.out.println("ArrayList insertion order: "+list);
+
+	System.out.println("Adding an element at a specific index after B element.");
+
+	list.add(2, "C");
+
+	System.out.println("ArrayList insertion order after adding C: "+list);
+
+	System.out.println("Adding an element at a specific index after E");
+
+	list.add(5, "F");
+
+	System.out.println("ArrayList insertion order after adding F: "+list); 
+	}
+
+	}
